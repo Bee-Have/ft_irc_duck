3ccb087ece30596cc5e786528d7e2c6af11f7ed7
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2023-05-29 14:32:01 +0200

    🎉✨🚧 -started implementing server class to build server

---
+++ srcs/server/server.cpp
@@ -0,0 +1,46 @@
+#include "server.hpp"
+
+server::server(void)
+{}
+
+server::server(int new_port, char *new_pass): _port(new_port), _pass(new_pass)
+{
+	_server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (_server_fd < 0)
+	{
+		std::cerr << "Error: server socket creation failed\n";
+		return ;
+	}
+	_server_addr.sin_family = AF_INET;
+	_server_addr.sin_port = htons(_port);
+	_server_addr.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind(_server_fd, (struct sockaddr *)&_server_addr, sizeof(_server_addr)) < 0)
+	{
+		std::cerr << "Error: bind socket to PORT failed\n";
+		return ;
+	}
+	if (listen(_server_fd, MAX_CLIENT) < 0)
+	{
+		std::cerr << "Error: socket listen failed\n";
+		return ;
+	}
+}
+
+server::server(const server &cpy)
+{}
+
+server::~server(void)
+{}
+
+server	&server::operator=(const server &assign)
+{
+	if (this != &assign)
+	{
+		_port = assign._port;
+		_pass.assign(assign._pass);
+		_server_fd = assign._server_fd;
+		_server_addr = assign._server_addr;
+	}
+	return (*this);
+}
---
+++ srcs/server/server.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define MAX_CLIENT 10
+
+class server
+{
+private:
+	server(const server &cpy);
+
+	int			_port;
+	std::string	_pass;
+
+	int					_server_fd;
+	struct sockaddr_in	_server_addr;
+
+public:
+	server();
+	server(int new_port, char *new_pass);
+	~server();
+
+	server	&operator=(const server &assign);
+};
