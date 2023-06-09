954f7c7eee8a857a997089a4d4e58c2e732117ef
amarini- <amarini-@Inception42.myguest.virtualbox.org>
2023-06-09 13:38:38 +0200

    🐛 -there was an error in the send message checks fo sending messages. i wrote "/n" instead of "\n", im dumb

--- srcs/messages/send_messages.cpp
+++ srcs/messages/send_messages.cpp
@@ -1,6 +1,24 @@
 #include "ircserv.hpp"
 
 /*
+	This function will check if there are any empty messages.
+	empty here means it has noone to send its message to
+*/
+static void	check_msgs_to_delete(server &serv)
+{
+	for (std::vector<message>::iterator it = serv.msgs.begin();
+		it != serv.msgs.end(); ++it)
+	{
+		if (it->target.empty() == true)
+		{
+			it = serv.msgs.erase(it);
+			if (serv.msgs.empty() == true)
+				return ;
+		}
+	}
+}
+
+/*
 	This function messages to the target (msgs->target) if :
 		-the target fd is present in write_fds (allowed list of fds given by select())
 		-the message contains an end of message : '\r\n'
@@ -10,17 +28,21 @@
 */
 void	send_messages(server &serv, fd_set &write_fds)
 {
+	check_msgs_to_delete(serv);
 	if (serv.msgs.empty() == true)
 		return ;
+	std::cout << "gonna send msg" << std::endl;
 	for (std::vector<message>::iterator it_msg = serv.msgs.begin();
 		it_msg != serv.msgs.end(); ++it_msg)
 	{
+		std::cout << "msg:" << it_msg->text << "|";
 		for (std::set<int>::iterator it_fd = it_msg->target.begin();
 			it_fd != it_msg->target.end(); ++it_fd)
 		{
 			if (FD_ISSET(*it_fd, &write_fds) != 0
-				&& it_msg->text.find("/r/n") != std::string::npos)
+				&& it_msg->text.find("\r\n") != std::string::npos)
 			{
+				std::cout << "about to send" << std::endl;
 				send(*it_fd, it_msg->text.c_str(), it_msg->text.size(), 0);
 				FD_CLR(*it_fd, &write_fds);
 				it_msg->target.erase(it_fd);
