e7f1023dcb02cc6a61f575ca55bcf21c4c01bb2f
Albe MARINI-AUDOUARD <amarini-@e3r10p7.clusters.42paris.fr>
2023-06-14 18:01:11 +0200

    🔥 -deleted ERR_NICKCOLLISION from errors header file, also updated todo as a result

--- srcs/errors.hpp
+++ srcs/errors.hpp
@@ -3,4 +3,3 @@
 #define ERR_NONICKNAMEGIVEN " :No nickname given\r\n"
 #define ERR_ERRONEUSNICKNAME " :Erroneus nickname\r\n"
 #define ERR_NICKNAMEINUSE " :Nickname is already in use\r\n"
-#define ERR_NICKCOLLISION " :Nickname collision KILL\r\n"
