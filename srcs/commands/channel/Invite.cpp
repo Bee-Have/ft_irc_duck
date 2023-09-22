954a731506bac63feabd00ae91a526505fa0f575
Bee-Have <52035659+Bee-Have@users.noreply.github.com>
2023-09-22 16:11:51 +0000

    🚧 -working on implementation of invite command

--- srcs/commands/channel/Invite.cpp
+++ srcs/commands/channel/Invite.cpp
@@ -5,20 +5,72 @@ Invite::Invite(Server &p_serv): ICommand(p_serv)
 
 void	Invite::execute(Message &msg)
 {
-	std::string	nickname;
-	std::string	channel;
+	std::vector<std::string>	success_reply(1, RPL_INVITE);
+	std::vector<std::string>	success_replace;
 
+	if (is_param_format_good(msg) == false)
+		return ;
+	if (do_param_exist_and_set_if_so(msg) == false)
+		return ;
+	if (are_param_membership_valid(msg) == false)
+		return ;
+
+	channel->_clients[]
+	success_replace.push_back(serv.client_list.find(client)->second.nickname);
+	success_replace.push_back(channel->_name);
+	msg.reply_format()
+}
+
+bool	Invite::is_param_format_good(Message &msg)
+{
 	if (msg.cmd_param.empty() == true)
-		return (msg.reply_format(ERR_NEEDMOREPARAMS, "INVITE", serv.socket_id));
+	{
+		msg.reply_format(ERR_NEEDMOREPARAMS, "INVITE", serv.socket_id);
+		return (false);
+	}
 	if (msg.cmd_param.find(' ') == std::string::npos)
-		return (msg.reply_format(ERR_NEEDMOREPARAMS, "INVITE", serv.socket_id));
+	{
+		msg.reply_format(ERR_NEEDMOREPARAMS, "INVITE", serv.socket_id);
+		return (false);
+	}
+	return (true);
+}
 
-	nickname = msg.cmd_param.substr(0, msg.cmd_param.find(' '));
-	channel = msg.cmd_param.substr(msg.cmd_param.find(' ') + 1, msg.cmd_param.size());
+bool	Invite::do_param_exist_and_set_if_so(Message &msg)
+{
+	std::string	nickname(msg.cmd_param.substr(0, msg.cmd_param.find(' ')));
+	std::string	chan_name(msg.cmd_param.substr(msg.cmd_param.find(' ') + 1, msg.cmd_param.size()));
 
-	std::cout << "NICKNAME [" << nickname << "]\n";
-	std::cout << "CHANNEL [" << channel << "]\n";
+	if (serv._channel_list.find(chan_name) == serv._channel_list.end())
+	{
+		msg.reply_format(ERR_NOSUCHCHANNEL, chan_name, serv.socket_id);
+		return (false);
+	}
+	if (serv.get_client_by_nickname(nickname) == -1)
+	{
+		msg.reply_format(ERR_NOSUCHNICK, nickname, serv.socket_id);
+		return (false);
+	}
+	channel = &serv._channel_list.find(chan_name)->second;
+	client = serv.get_client_by_nickname(nickname);
+	return (true);
+}
 
-	if (serv._channel_list.find(channel) == serv._channel_list.end())
-		return (msg.reply_format(ERR_NOSUCHCHANNEL));
+bool	Invite::are_param_membership_valid(Message &msg)
+{
+	if (channel->_clients.find(client) == channel->_clients.end())
+	{
+		std::vector<std::string>	reply(1, ERR_USERONCHANNEL);
+		std::vector<std::string>	replace;
+		replace.push_back(serv.client_list.find(client)->second.nickname);
+		replace.push_back(channel->_name);
+		msg.reply_format(reply, replace);
+		return (false);
+	}
+	if (channel->_clients.find(msg.get_emitter()) == channel->_clients.end())
+	{
+		msg.reply_format(ERR_NOTONCHANNEL, channel->_name, serv.socket_id);
+		return (false);
+	}
+	return (true);
 }
--- srcs/commands/channel/Invite.hpp
+++ srcs/commands/channel/Invite.hpp
@@ -4,8 +4,15 @@
 
 struct Invite : public ICommand
 {
+	Channel *channel;
+	int		client;
+
 	Invite(Server &p_serv);
 
 	void	execute(Message &msg);
+
+	bool	is_param_format_good(Message &msg);
+	bool	do_param_exist_and_set_if_so(Message &msg);
+	bool	are_param_membership_valid(Message &msg);
 };
 
