98abf9770ab47f981319bf59cac8457002448d53
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-10-07 01:26:10 +0200

    🧑‍💻 OPER parsing is more accepting.

--- srcs/commands/management/Oper.cpp
+++ srcs/commands/management/Oper.cpp
@@ -8,12 +8,17 @@ void	Oper::execute(Message &msg)
 	std::string	oper;
 	std::string	pass;
 	std::string	return_msg;
+	size_t oper_end_pos = msg.cmd_param.find(' ');
+	size_t pass_begin_pos;
 
-	if (msg.cmd_param.find(' ') == std::string::npos)
-		return (msg.reply_format(ERR_PASSWDMISMATCH, "", serv.socket_id));
+	oper = msg.cmd_param.substr(0, oper_end_pos);
 
-	oper = msg.cmd_param.substr(0, msg.cmd_param.find(' '));
-	pass = msg.cmd_param.substr(msg.cmd_param.find(' ') + 1, msg.cmd_param.size());
+	if (oper_end_pos == std::string::npos)
+		return (msg.reply_format(ERR_NEEDMOREPARAMS, "OPER", serv.socket_id));
+	pass_begin_pos = msg.cmd_param.find_first_not_of(" \t", oper_end_pos);
+	if (pass_begin_pos == std::string::npos)
+		return (msg.reply_format(ERR_NEEDMOREPARAMS, "OPER", serv.socket_id));
+	pass = msg.cmd_param.substr(pass_begin_pos);
 
 	return_msg = serv.oper_command_check(msg.get_emitter(), oper, pass);
 
